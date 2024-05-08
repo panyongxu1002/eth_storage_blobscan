cf5361b7bb1aedebbfff12e97c170466cb7883ff
finch ren <344919881@qq.com>
2024-05-08 13:18:28 +0800

    fix: iframe style

--- apps/web/src/pages/monitoring/index.tsx
+++ apps/web/src/pages/monitoring/index.tsx
@@ -3,12 +3,11 @@ import type { NextPage } from "next";
 const Monitoring: NextPage = () => {
   return (
     <iframe
-      title="Next page"
+      title="ethstorage"
       src="https://grafana.ethstorage.io/d-solo/fdfcc57c-30fb-424e-8dc5-b0cf7bcce84b/ethstorage-monitoring?orgId=2&refresh=5m&from=now-1m&to=now&panelId=13"
-      width="450"
-      height="200"
-      frameBorder="0"
-    ></iframe>
+      width="100%"
+      height="600"
+    />
   );
 };
 
